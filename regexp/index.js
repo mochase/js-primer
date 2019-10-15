3c95c2f07db0df2afd57b7d98e47bb1a88d47147
zheyuan.dai <zheyuan.dai@percent.cn>
2019-10-15 14:02:47 +0800

    一些设计模式更新

--- regexp/index.js
+++ regexp/index.js
@@ -3,3 +3,10 @@ const g1 = /<[^/e].*?>|<\/[^e].*?>/gi
 
 // 去除所有html标签
 const g2 = /<[^>]+>/gi
+
+// 格式化网页,抽取文本友好展示
+function prettyFormatText (str) {
+    const regex = /<script.*?<\/script>|<style.*?<\/style>|<noscript.*?<\/noscript>|<code.*?<\/code>|<textarea.*?<\/textarea>|<link.*?>|<[^>]+>|&lt;|&gt;/gsi;
+    let tmp = str.replace(regex, '')
+    return tmp.replace(/( |\t|\v|\f)*\n( |\t|\v|\f)*/gs, '\n').replace(/\n{2,}/gs, '\n')
+}
